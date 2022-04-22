eedcb5b74e71f52402ac44bf9cff7f8929c22190
Bohdan <stetsenko.bogdan.work@gmail.com>
2022-04-22 11:11:37 +0300

    Project creation; Adding a directory with images; Connecting the necessary packages; Creating a website header

---
+++ project/src/components/app/App.js
@@ -0,0 +1,15 @@
+import AppHeader from "../appHeader/AppHeader";
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="app-wrapper">
+        <div className="container">
+          <AppHeader />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
